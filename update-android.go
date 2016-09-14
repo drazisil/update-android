437ea9c4a79a2f6ac12f7ef2c26a5e40a1ff23f9
joseph w becher <joseph@circleci.com>
2016-09-14 16:43:52 -0400

    Trying a function for testing

--- update-android.go
+++ update-android.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Printf("hello, world\n")
+    fmt.Printf(Hello())
+}
+
+func Hello() {
+	return hello, world\n"
 }
---
+++ update-android_test.go
@@ -0,0 +1,17 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	cases := []struct {
+		in, want string
+	}{
+		{"hello, world\n", "\ndlrow ,olleh"},
+	}
+	for _, c := range cases {
+		got := Hello(c.in)
+		if got != c.want {
+			t.Errorf("Hello(%q) == %q, want %q", c.in, got, c.want)
+		}
+	}
+}
