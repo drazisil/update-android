3b7936e89d39d9a39d83bdd462fb7c89507de604
joseph w becher <joseph@circleci.com>
2016-09-14 17:34:28 -0400

    Trying to get a url

--- update-android.go
+++ update-android.go
@@ -1,9 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-    fmt.Printf(Hello())
+	addonListUrl := "https://dl.google.com/android/repository/addons_list-2.xml"
+
+	if len(os.Args) != 2 {
+        fmt.Fprintf(os.Stderr, "Usage: %s URL\n", os.Args[0])
+        os.Exit(1)
+    }
+
+    response, err := http.Get(addonListUrl)
+        if err != nil {
+                log.Fatal(err)
+        } else {
+                defer response.Body.Close()
+                _, err := io.Copy(os.Stdout, response.Body)
+                if err != nil {
+                        log.Fatal(err)
+                }
+        }
 }
 
 func Hello() string {
