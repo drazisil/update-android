41bb467b7ddd26e1b71d0e7027b6a793c4ac9f0c
joseph w becher <joseph@circleci.com>
2016-09-14 22:26:14 -0400

    Saving for now, too tired to continue

--- update-android.go
+++ update-android.go
@@ -1,54 +1,59 @@
 package main
 
 import (
-	"fmt"
-    "io"
+    "encoding/xml"
+    "fmt"
     "log"
     "net/http"
     "os"
-    "encoding/xml"
 )
 
-type Query struct {
-    //Series Show
-    // Have to specify where to find episodes since this
-    // doesn't match the xml tags of the data that needs to go into it
-    AddonSiteList []AddonSite `xml:"sdk:addon-site>"`
+type AddonsLists struct {
+    XMLName xml.Name  `xml:"http://schemas.android.com/sdk/android/addons-list/2 sdk-addons-list"`
+    AddonSite  []*AddonSite `xml:"http://schemas.android.com/sdk/android/addons-list/2 sdk-addon-site"`
+    SysImgSite  []*SysImgSite `xml:"http://schemas.android.com/sdk/android/addons-list/2 sys-img-site"`
 }
 
 type AddonSite struct {
-     Name  `xml:"sdk:name`
-    Url   string   `xml:"sdk:url`
+     Name string `xml:"http://schemas.android.com/sdk/android/addons-list/2 name"`
+     Url string `xml:"http://schemas.android.com/sdk/android/addons-list/2 url"`
+}
+
+type SysImgSite struct {
+     Name string `xml:"http://schemas.android.com/sdk/android/addons-list/2 name"`
+     Url string `xml:"http://schemas.android.com/sdk/android/addons-list/2 url"`
 }
 
 //        <sdk:name>Google Inc.</sdk:name>
 //        <sdk:url>addon.xml</sdk:url>
 
 func main() {
-	addonListUrl := "https://dl.google.com/android/repository/addons_list-2.xml"
+    addonListUrl := "https://dl.google.com/android/repository/addons_list-2.xml"
 
-	if len(os.Args) != 2 {
-        fmt.Fprintf(os.Stderr, "Usage: %s URL\n", os.Args[0])
-        os.Exit(1)
-    }
+//  if len(os.Args) != 2 {
+//        fmt.Fprintf(os.Stderr, "Usage: %s URL\n", os.Args[0])
+//        os.Exit(1)
+//    }
 
     response, err := http.Get(addonListUrl)
         if err != nil {
-                log.Fatal(err)
+            log.Fatal(err)
         } else {
-                defer response.Body.Close()
+            defer response.Body.Close()
                 
-                var q Query
-                xmlFile := response.Body
-                xml.Unmarshal(xmlFile, &q)
-                
-                _, err := io.Copy(os.Stdout, response.Body)
-                if err != nil {
-                        log.Fatal(err)
-                }
+            var q AddonsLists
+            xmlFile := response.Body
+            if err := xml.NewDecoder(xmlFile).Decode(&q); err != nil {
+                log.Fatal(err)
+                os.Exit(1)
+            }
+
+            // Display The first strap
+            fmt.Printf("%#v\n", q)
+            //fmt.Printf("Key: %s  Value: %s", q[0].Key, q[0].Value)
         }
 }
 
 func Hello() string {
-	return string("hello, world\n")
+    return string("hello, world\n")
 }
