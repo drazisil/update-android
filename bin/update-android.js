9b9c21fee8f4346dfe5c8fd7699306995abbb361
joseph w becher <joseph@circleci.com>
2016-09-15 21:00:28 -0400

    Prepping for testing

--- bin/update-android.js
+++ bin/update-android.js
@@ -8,14 +8,36 @@ var program = require('commander');
 
 program
   .version('0.0.1')
-  .option('-p, --peppers', 'Add peppers')
-  .option('-P, --pineapple', 'Add pineapple')
-  .option('-b, --bbq-sauce', 'Add bbq sauce')
-  .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
-  .parse(process.argv);
+  // .option('-C, --chdir <path>', 'change the working directory')
+  // .option('-c, --config <path>', 'set config path. defaults to ./deploy.conf')
+  // .option('-T, --no-tests', 'ignore test hook')
 
-console.log('you ordered a pizza with:');
-if (program.peppers) console.log('  - peppers');
-if (program.pineapple) console.log('  - pineapple');
-if (program.bbqSauce) console.log('  - bbq');
-console.log('  - %s cheese', program.cheese);
+program
+  .command('list sdk')
+  .description('run setup commands for all envs')
+  .option("-s, --setup_mode [mode]", "Which setup mode to use")
+  .action(function(env, options){
+    var mode = options.setup_mode || "normal";
+    env = env || 'all';
+    console.log('setup for %s env(s) with %s mode', env, mode);
+  });
+
+program
+  .command('update <cmd>')
+  .alias('ex')
+  .description('execute the given remote cmd')
+  .option("-e, --exec_mode <mode>", "Which exec mode to use")
+  .action(function(cmd, options){
+    console.log('exec "%s" using %s mode', cmd, options.exec_mode);
+  }).on('--help', function() {
+    console.log('  Examples:');
+    console.log();
+    console.log('    $ deploy exec sequential');
+    console.log('    $ deploy exec async');
+    console.log()
+  });
+
+program.parse(process.argv)
+
+// If no commands here entered, display help
+program.help()
