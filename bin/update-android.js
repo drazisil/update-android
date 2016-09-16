40489222c0c0fc6e4e7edcb3d909013254326dd4
joseph w becher <joseph@circleci.com>
2016-09-16 10:45:51 -0400

    work on help and start of sdk

--- bin/update-android.js
+++ bin/update-android.js
@@ -6,18 +6,5 @@
 
 var updateAndroid = require('../lib/index.js')
 var packageJson = require('../package.json')
-const commandLineCommands = require('command-line-commands')
  
-const validCommands = [ null, 'list', 'update', 'help', 'version' ]
-try {
-	const { command, argv } = commandLineCommands(validCommands)
-  updateAndroid.processCmd(command, JSON.stringify(argv))
-} catch (err) {
-  if (err.name === 'INVALID_COMMAND') {
-    console.log('moo')
-    process.exit()
-  } else {
-    console.dir(err)
-    process.exit(1)
-  }
-}
+updateAndroid.processCmd(updateAndroid.parseCmds(process.argv))
--- test/index_test.js
+++ test/index_test.js
@@ -1,8 +1,18 @@
 var should = require('chai').should()
 var updateAndroid = require('../lib/index.js')
 
+describe('check functions', function() {
+  it('parseArgs() should remove first two values from an array', function(done) {
+    updateAndroid.parseCmds(['a', 'b', 'c', 'd']).should.eql(['c', 'd'])
+    done();
+  })
+  it('parseArgs() should return empty array if passed only two values', function(done) {
+    updateAndroid.parseCmds(['a', 'b']).should.eql([])
+    done();
+  })})
+
 describe('check urls', function() {
-  it('should have a valid root xml url', function(done) {
+  it('addonListUrl should be a valid root xml url', function(done) {
     updateAndroid.addonListUrl.should.equal('https://dl.google.com/android/repository/addons_list-2.xml')
     done();
   })
