7a93cf899707c4d3aa8eb0f1d76305d1d0d50534
joseph w becher <joseph@circleci.com>
2016-09-18 12:45:03 -0400

    Does this even work?

--- src/update_android/core.clj
+++ src/update_android/core.clj
@@ -1,20 +1,70 @@
 (ns update-android.core
-  (:require [clojure.tools.cli :refer [parse-opts]])
+  (:require [clojure.string :as string]
+            [clojure.tools.cli :refer [parse-opts]])
+  (:import (java.net InetAddress))
   (:gen-class))
 
 (def cli-options
-  ;; An option with a required argument
-  [["-p" "--port PORT" "Port number"
+  [;; First three strings describe a short-option, long-option with optional
+   ;; example argument description, and a description. All three are optional
+   ;; and positional.
+   ["-p" "--port PORT" "Port number"
     :default 80
     :parse-fn #(Integer/parseInt %)
     :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]]
-   ;; A non-idempotent option
-   ["-v" nil "Verbosity level"
+   ["-H" "--hostname HOST" "Remote host"
+    :default (InetAddress/getByName "localhost")
+    ;; Specify a string to output in the default column in the options summary
+    ;; if the default value's string representation is very ugly
+    :default-desc "localhost"
+    :parse-fn #(InetAddress/getByName %)]
+   ;; If no required argument description is given, the option is assumed to
+   ;; be a boolean option defaulting to nil
+   [nil "--detach" "Detach from controlling process"]
+   ["-v" nil "Verbosity level; may be specified multiple times to increase value"
+    ;; If no long-option is specified, an option :id must be given
     :id :verbosity
     :default 0
+    ;; Use assoc-fn to create non-idempotent options
     :assoc-fn (fn [m k _] (update-in m [k] inc))]
-   ;; A boolean option defaulting to nil
    ["-h" "--help"]])
 
+(defn usage [options-summary]
+  (->> ["This is my program. There are many like it, but this one is mine."
+        ""
+        "Usage: program-name [options] action"
+        ""
+        "Options:"
+        options-summary
+        ""
+        "Actions:"
+        "  start    Start a new server"
+        "  stop     Stop an existing server"
+        "  status   Print a server's status"
+        ""
+        "Please refer to the manual page for more information."]
+       (string/join \newline)))
+
+(defn error-msg [errors]
+  (str "The following errors occurred while parsing your command:\n\n"
+       (string/join \newline errors)))
+
+(defn moo [errors]
+  (str "The following occurred while parsing your command:\n\n"
+       (string/join \newline "moo")))
+
+(defn exit [status msg]
+  (println msg)
+  (System/exit status))
+
 (defn -main [& args]
-  (parse-opts args cli-options))
+  (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)]
+    ;; Handle help and error conditions
+    (cond
+      (:help options) (exit 0 (usage summary))
+      (not= (count arguments) 1) (exit 1 (usage summary))
+      errors (exit 1 (error-msg errors)))
+    ;; Execute program with options
+    (case (first arguments)
+      "moo" (moo options)
+      (exit 1 (usage summary)))))
