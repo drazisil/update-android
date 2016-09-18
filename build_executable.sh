3942d0e93c793e4a4a5e824d10ae9ddc1bf1abc6
joseph w becher <joseph@circleci.com>
2016-09-18 12:13:13 -0400

    Corrected path to standalone jar

--- build_executable.sh
+++ build_executable.sh
@@ -2,4 +2,4 @@
 #!/bin/bash
 
 cp cmdlinetool_template update-android
-cat ./target/update-android-0.1.0-snapshot.jar >> update-android
+cat ./target/uberjar/update-android-0.1.0-SNAPSHOT-standalone.jar >> update-android
