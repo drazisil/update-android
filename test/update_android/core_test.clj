dc99bd657ed26b04918bdb960f63b99c16afeb66
joseph w becher <joseph@circleci.com>
2016-09-18 12:11:06 -0400

    fixed "test"

--- test/update_android/core_test.clj
+++ test/update_android/core_test.clj
@@ -3,5 +3,5 @@
             [update-android.core :refer :all]))
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "FIXME, I pass, but I'm pointless ;)."
+    (is (= 1 1))))
