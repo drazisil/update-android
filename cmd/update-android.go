d596e06fccdb2229fe5cce02264d7c1fb3cf63d0
joseph w becher <joseph@circleci.com>
2016-10-10 11:13:15 -0400

    package main is now a wrapper that has nothing testable

--- cmd/update-android.go
+++ cmd/update-android.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/xml"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,25 +8,6 @@ import (
 
 	"github.com/drazisil/updateandroid"
 )
-
-type AddonsLists struct {
-	XMLName    xml.Name      `xml:"http://schemas.android.com/sdk/android/addons-list/2 sdk-addons-list"`
-	AddonSite  []*AddonSite  `xml:"http://schemas.android.com/sdk/android/addons-list/2 sdk-addon-site"`
-	SysImgSite []*SysImgSite `xml:"http://schemas.android.com/sdk/android/addons-list/2 sys-img-site"`
-}
-
-type AddonSite struct {
-	Name string `xml:"http://schemas.android.com/sdk/android/addons-list/2 name"`
-	Url  string `xml:"http://schemas.android.com/sdk/android/addons-list/2 url"`
-}
-
-type SysImgSite struct {
-	Name string `xml:"http://schemas.android.com/sdk/android/addons-list/2 name"`
-	Url  string `xml:"http://schemas.android.com/sdk/android/addons-list/2 url"`
-}
-
-//        <sdk:name>Google Inc.</sdk:name>
-//        <sdk:url>addon.xml</sdk:url>
 
 func main() {
 
@@ -39,18 +19,8 @@ func main() {
 	response, err := http.Get(updateandroid.AddonsListUrl())
 	if err != nil {
 		log.Fatal(err)
-	} else {
-		defer response.Body.Close()
-
-		var q AddonsLists
-		xmlFile := response.Body
-		if err := xml.NewDecoder(xmlFile).Decode(&q); err != nil {
-			log.Fatal(err)
-			os.Exit(1)
-		}
-
-		// Display The first strap
-		fmt.Printf("%#v\n", q)
-		//fmt.Printf("Key: %s  Value: %s", q[0].Key, q[0].Value)
+		os.Exit(1)
 	}
+
+	fmt.Printf("%#v\n", response)
 }
--- repository.go
+++ repository.go
@@ -1,10 +1,58 @@
 package updateandroid
 
+import (
+	"encoding/xml"
+	"log"
+	"net/http"
+	"os"
+)
+
 var googleRepositoryVersion = "12"
 var googleBaseUrl = "https://dl.google.com/android/repository/"
 var googleRepositoryUrl = googleBaseUrl + "repository-" + googleRepositoryVersion + " .xml"
 var googleAddonsListUrl = googleBaseUrl + "addons_list-2.xml"
 
+type AddonsLists struct {
+	XMLName    xml.Name      `xml:"http://schemas.android.com/sdk/android/addons-list/2 sdk-addons-list"`
+	AddonSite  []*addonSite  `xml:"http://schemas.android.com/sdk/android/addons-list/2 sdk-addon-site"`
+	SysImgSite []*sysImgSite `xml:"http://schemas.android.com/sdk/android/addons-list/2 sys-img-site"`
+}
+
+type addonSite struct {
+	Name string `xml:"http://schemas.android.com/sdk/android/addons-list/2 name"`
+	Url  string `xml:"http://schemas.android.com/sdk/android/addons-list/2 url"`
+}
+
+type sysImgSite struct {
+	Name string `xml:"http://schemas.android.com/sdk/android/addons-list/2 name"`
+	Url  string `xml:"http://schemas.android.com/sdk/android/addons-list/2 url"`
+}
+
+//        <sdk:name>Google Inc.</sdk:name>
+//        <sdk:url>addon.xml</sdk:url>
+
 func AddonsListUrl() string {
 	return googleAddonsListUrl
+}
+
+func Init() ([]AddonsLists, error) {
+	var q []AddonsLists
+
+	response, err := http.Get(AddonsListUrl())
+	if err != nil {
+		return nil, err
+	} else {
+		defer response.Body.Close()
+
+		xmlFile := response.Body
+		if err := xml.NewDecoder(xmlFile).Decode(&q); err != nil {
+			log.Fatal(err)
+			os.Exit(1)
+		}
+	}
+
+	return q, nil
+	// Display The first strap
+	//fmt.Printf("%#v\n", q)
+	//fmt.Printf("Key: %s  Value: %s", q[0].Key, q[0].Value)
 }
